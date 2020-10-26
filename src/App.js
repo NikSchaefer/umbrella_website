322f642d8d168c43e3dccd55d91078f491e87de2
Nik Schaefer <nikkschaefer@gmail.com>
2020-10-25 22:22:00 -0500

    Inital

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import Home from './Pages/Home'
+import NavBar from './Pages/NavBar'
+import Footer from './Pages/Footer'
+
+import {BrowserRouter,Route,} from "react-router-dom";
+
+
 function App() {
+
+
+  
+
+
   return (
+    <BrowserRouter>
+      
+      
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <NavBar />
+      <Route exact path='/' component={Home} />
+      <Footer />
+
     </div>
+    </BrowserRouter>
   );
 }
 
